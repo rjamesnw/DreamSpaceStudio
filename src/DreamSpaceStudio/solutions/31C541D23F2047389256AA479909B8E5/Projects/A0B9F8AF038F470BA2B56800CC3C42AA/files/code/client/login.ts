049d7adb8773be216f5f9dc6437fd36c5e950610
James <contactme@jameswilkins.net>
2019-07-11 13:57:17 -0400

    ...

--- src/DreamSpaceStudio/solutions/31C541D23F2047389256AA479909B8E5/Projects/A0B9F8AF038F470BA2B56800CC3C42AA/files/code/client/login.ts
+++ src/DreamSpaceStudio/solutions/31C541D23F2047389256AA479909B8E5/Projects/A0B9F8AF038F470BA2B56800CC3C42AA/files/code/client/login.ts
@@ -1,4 +1,5 @@
 async function login(username: string, password: string): Promise<IResponse<boolean>> {
     // ... takes the username and password given from the *UI* and attempts to login the user ...
-    return await DS.get<IResponse<boolean>>("api/login", "json", "POST", { username, password });
-}
+
+    var result = await DS.get<IResponse<boolean>>("api/login", "json", "POST", { username, password });
+

b5d292b820e5baf2681d9734f3871f81be1b4d96
James <contactme@jameswilkins.net>
2021-02-02 00:23:32 -0500

    Implemented requireJS for loading modules async on the client side (on demand, so only what is needed).

---
+++ src/DreamSpaceStudio/features/mirandus/Manager.ts
@@ -0,0 +1,67 @@
+export class Deed {
+}
+
+/** The place upon which deeds are placed. */
+export class Land {
+    defaultColor: "#A0E031";
+    defaultTexture: ""; // (link to a grass image perhaps)
+    showGrid: true;
+    pixelsPerBlock: 16;
+
+    constructor(target: string | HTMLElement) {
+        alert(target);
+    }
+
+    addDeed(deed: Deed, x: number, y: number) {
+
+    }
+}
+
+export enum PlotTypes {
+    Main, // (the main building for the yellow plot)
+    Farming,
+    Ranching,
+    General,
+    Decoration
+}
+
+export interface IPlot {
+    x: number; y: number;
+    w: number; h: number;
+    type: PlotTypes;
+}
+
+export class Homestead extends Deed {
+    private _layout: IPlot[] = [
+        { x: 0, y: 0, w: 10, h: 10, type: PlotTypes.Main },
+        { x: 11, y: 0, w: 5, h: 5, type: PlotTypes.Farming },
+        { x: 11, y: 6, w: 5, h: 5, type: PlotTypes.General },
+        { x: 1, y: 11, w: 2, h: 2, type: PlotTypes.Decoration }
+    ];
+
+    readonly land: Land;
+
+    render(target: string | HTMLElement) {
+        var _target = typeof target == 'object' ? target : document.getElementById('' + target);
+        if (_target) {
+            var container = document.createElement("div");
+            container.style.display = "relative";
+            for (var plot of this._layout) {
+                var plotEl = document.createElement("div");
+                plotEl.style.left = (plot.x * this.land.pixelsPerBlock) + "px";
+                plotEl.style.top = (plot.y * this.land.pixelsPerBlock) + "px";
+                plotEl.style.width = (plot.w * this.land.pixelsPerBlock) + "px";
+                plotEl.style.height = (plot.h * this.land.pixelsPerBlock) + "px";
+                container.appendChild(plotEl);
+            }
+        }
+    }
+}
+
+export class Manager {
+    land: Land;
+
+    constructor(topbarID: string, outputID: string) {
+        this.land = new Land(outputID);
+    }
+}
--- src/DreamSpaceStudio/public/js/features/mirandus/index.js
+++ src/DreamSpaceStudio/public/js/features/mirandus/index.js
@@ -1,50 +1,6 @@
-class Deed {
-}
-/** The place upon which deeds are placed. */
-class Land {
-    constructor(target) {
-    }
-    addDeed(deed, x, y) {
-    }
-}
-var PlotTypes;
-(function (PlotTypes) {
-    PlotTypes[PlotTypes["Main"] = 0] = "Main";
-    PlotTypes[PlotTypes["Farming"] = 1] = "Farming";
-    PlotTypes[PlotTypes["Ranching"] = 2] = "Ranching";
-    PlotTypes[PlotTypes["General"] = 3] = "General";
-    PlotTypes[PlotTypes["Decoration"] = 4] = "Decoration";
-})(PlotTypes || (PlotTypes = {}));
-class Homestead extends Deed {
-    constructor() {
-        super(...arguments);
-        this._layout = [
-            { x: 0, y: 0, w: 10, h: 10, type: PlotTypes.Main },
-            { x: 11, y: 0, w: 5, h: 5, type: PlotTypes.Farming },
-            { x: 11, y: 6, w: 5, h: 5, type: PlotTypes.General },
-            { x: 1, y: 11, w: 2, h: 2, type: PlotTypes.Decoration }
-        ];
-    }
-    render(target) {
-        var _target = typeof target == 'object' ? target : document.getElementById('' + target);
-        if (_target) {
-            var container = document.createElement("div");
-            container.style.display = "relative";
-            for (var plot of this._layout) {
-                var plotEl = document.createElement("div");
-                plotEl.style.left = (plot.x * this.land.pixelsPerBlock) + "px";
-                plotEl.style.top = (plot.y * this.land.pixelsPerBlock) + "px";
-                plotEl.style.width = (plot.w * this.land.pixelsPerBlock) + "px";
-                plotEl.style.height = (plot.h * this.land.pixelsPerBlock) + "px";
-                container.appendChild(plotEl);
-            }
-        }
-    }
-}
-class Manager {
-    constructor() {
-        this.land = new Land("output");
-    }
-}
-alert("!");
+define(["require", "exports", "./Manager"], function (require, exports, Manager_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var manager = new Manager_1.Manager("topBar", "output");
+});
 //# sourceMappingURL=index.js.map
